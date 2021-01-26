53263125263f5df03fc4c86e7171ae3d3fdf142c
JeremyDallain <jeremy.dallain@gmail.com>
2021-01-26 10:23:16 +0100

    test

---
+++ modele.php
@@ -0,0 +1,12 @@
+<?php
+$titlePage = "titre de la page";
+$h1 = "le titre affiché dans le H1";
+require_once 'commons/header.php';
+?>
+<!-- DEBUT CONTENU DE LA PAGE -->
+
+
+
+<!-- FIN CONTENU DE LA PAGE -->
+<?php
+require_once 'commons/footer.php';
